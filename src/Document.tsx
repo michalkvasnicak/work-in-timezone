577bb7014ac640d5d35f21a26b7f878bc1554dfd
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-12-02 10:04:51 +0100

    add google analytics

--- src/Document.tsx
+++ src/Document.tsx
@@ -7,10 +7,28 @@ interface DocumentProps {
   js: string;
 }
 
+const ANALYTICS_TRACKING_ID = process.env.RAZZLE_ANALYTICS_TRACKING_ID || '';
+const gtag = `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', '${ANALYTICS_TRACKING_ID}');
+`;
+
 export function Document({ content, helmet, js }: DocumentProps) {
   return (
     <html lang="en" {...helmet.htmlAttributes.toComponent()}>
       <head>
+        {ANALYTICS_TRACKING_ID ? (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${ANALYTICS_TRACKING_ID}`}
+            />
+            <script dangerouslySetInnerHTML={{ __html: gtag }} />
+          </>
+        ) : null}
         <meta charSet="UTF-8" />
         <meta property="og:type" content="website" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
