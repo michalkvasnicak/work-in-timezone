49250373bbc61cc152c03e3f821d99093ffb434c
Michal Kvasničák <michal.kvasnicak@gmail.com>
2019-12-02 08:47:35 +0100

    remove serverless-http and export server

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,37 @@
 /* eslint-disable import/no-mutable-exports, import/no-unresolved, global-require */
 import http from 'http';
-import serverless from 'serverless-http';
 import { SERVER_PORT } from './config';
 
 let app = require('./server').default;
 
 let currentApp = app;
 
-if (process.env.SERVERLESS) {
-  module.exports.handler = serverless(app);
-} else {
-  const server = http.createServer(app);
-  const port = SERVER_PORT;
+const server = http.createServer(app);
+const port = SERVER_PORT;
 
-  server
-    .listen(port, () => {
-      console.log(`🚀 started at http://localhost:${port}`);
-    })
-    .on('error', error => {
-      console.log(error);
-    });
+server
+  .listen(port, () => {
+    console.log(`🚀 started at http://localhost:${port}`);
+  })
+  .on('error', error => {
+    console.log(error);
+  });
 
-  if (module.hot) {
-    console.log('✅  Server-side HMR Enabled!');
+if (module.hot) {
+  console.log('✅  Server-side HMR Enabled!');
 
-    module.hot.accept('./server', () => {
-      console.log('🔁  HMR Reloading `./server`...');
+  module.hot.accept('./server', () => {
+    console.log('🔁  HMR Reloading `./server`...');
 
-      try {
-        app = require('./server').default;
-        server.removeListener('request', currentApp);
-        server.on('request', app);
-        currentApp = app;
-      } catch (error) {
-        console.error(error);
-      }
-    });
-  }
+    try {
+      app = require('./server').default;
+      server.removeListener('request', currentApp);
+      server.on('request', app);
+      currentApp = app;
+    } catch (error) {
+      console.error(error);
+    }
+  });
 }
+
+export default server;
